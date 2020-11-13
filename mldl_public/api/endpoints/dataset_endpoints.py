2607664f323752b96047470cffc5385a1738cc61
Zach Wade <zwade@zensors.com>
2020-11-13 10:13:33 -0500

    Add tmpfile caching to the streaming endpoint
    Change-Id: I8093708b6fbc26bc98de3f0b94e2b710c2873b0d

--- mldl_public/api/endpoints/dataset_endpoints.py
+++ mldl_public/api/endpoints/dataset_endpoints.py
@@ -1,7 +1,13 @@
+import random
+import json
+import tempfile
+from os import path, makedirs
 from typing import Any, Dict, Generator, List
 
 from .request import ApiNamespace
 from ..types import JsonDatasetVersion, JsonDataset
+
+process_directory = tempfile.mkdtemp(prefix="mldl-")
 
 class Dataset(ApiNamespace):
     def list(self, database_uid: str) -> List[JsonDataset]:
@@ -14,8 +20,26 @@ class Dataset(ApiNamespace):
         if chunk < 0 or chunk >= nchunks:
             raise Exception(f"Invalid chunk specification. {chunk} must be in the range [0, {nchunks})")
 
-        yield from self.stream[Dict[str, Any]](
+        dir_name = f"{process_directory}/{dataset_uid}-{split}-{nchunks}"
+        file_name = f"{dir_name}/chunk-{chunk}.jsonl"
+        makedirs(dir_name, exist_ok=True)
+
+        if path.exists(file_name):
+            with open(file_name, "r") as f:
+                for line in f.readlines():
+                    line = line.strip()
+                    if line == "":
+                        continue
+                    yield json.loads(line)
+            return
+
+        stream = self.stream[Dict[str, Any]](
             f"/database/{database_uid}/dataset/{dataset_uid}/split/{split}/stream",
             { "chunk": str(chunk), "nchunks": str(nchunks) }
         )
 
+        with open(file_name, "w+") as f:
+            for element in stream:
+                f.write(json.dumps(element))
+                f.write("\n")
+                yield element

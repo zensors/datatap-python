6726541384daf40b495c40aa05f9546c1cc68004
Zach Wade <zwade@zensors.com>
2021-03-17 10:58:54 -0400

    Abstract out the caching generator
    Change-Id: I4a7f666f11d2eb8f1ac98b534aab27789dc889ec

--- datatap/utils/__init__.py
+++ datatap/utils/__init__.py
@@ -4,6 +4,7 @@ A collection of primarily internal-use utilities.
 
 from .environment import Environment
 from .helpers import assert_one, DeletableGenerator
+from .cache_generator import CacheGenerator
 from .or_nullish import OrNullish
 from .print_helpers import basic_repr, color_repr, force_pretty_print, pprint, pprints
 
@@ -11,6 +12,7 @@ __all__ = [
 	"Environment",
 	"assert_one",
 	"DeletableGenerator",
+	"CacheGenerator",
 	"OrNullish",
 	"basic_repr",
 	"color_repr",
